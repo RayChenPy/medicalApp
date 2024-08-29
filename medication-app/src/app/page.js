e89c47ca56cf8238a7b548eb6c4c97ea142eff8e
MichaelC609 <mjcastillo@cpp.edu>
2024-08-29 14:04:09 -0700

    added navbar to home

--- medication-app/src/app/page.js
+++ medication-app/src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../../components/navbar";
+import Home from "../../pages/home";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      <Home />
     </>
   );
 }
