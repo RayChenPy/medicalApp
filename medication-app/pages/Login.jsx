ac3ca4b03afb51f0259d07646ad90a412f5ed03a
MichaelC609 <mjcastillo@cpp.edu>
2024-08-29 21:58:07 -0700

    created login form

---
+++ medication-app/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../pages/Login.css'
+
+const Login = () => {
+	return (
+		<>
+			<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+			
+			<div className='wrapper'>
+				<h1>Login</h1>
+				<form>
+					<div className='input-box'>
+						<input type='text' 
+						placeholder='Username' required />
+					</div>
+
+					<div className='input-box'>
+						<input type='password' 
+						placeholder='Password' required />
+					</div>
+
+					<button className='btn' 
+					type='submit'>Login</button>
+
+					<div className='signUp-link'>
+						<p>Don't have an account?  
+							<a href='#'> Sign up here!</a></p>
+					</div>
+				</form>
+			</div>
+		</>
+	)
+}
+
+export default Login
--- medication-app/src/app/page.js
+++ medication-app/src/app/page.js
@@ -2,11 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../../components/navbar";
 import Home from "../../pages/Home";
+import Login from "../../pages/Login";
 
 export default function home() {
   return (
     <>
-      <Home />
+      <Login />
     </>
   );
 }
